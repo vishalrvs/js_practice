6a7330b43f4c81352d81b8280bb5b22de45f6193
vishalrvs <32324136+vishalrvs@users.noreply.github.com>
2024-10-04 08:52:27 +0000

    Arithmetic tricky operarations

--- basics/3_conversionOperation.js
+++ basics/3_conversionOperation.js
@@ -3,16 +3,51 @@
 // let score = "33abc" // After conversion it appear as NaN
 // let score = null // After conversion it appear as 0
 // let score = undefined // After conversion it appear as NaN
-let score = true // After conversion it appear as 1 and false as 0
-console.table([score,typeof score])
-let newScore = Number(score)
-console.table([newScore,typeof newScore])
+// let score = true // After conversion it appear as 1 and false as 0
+
+// console.table([score,typeof score])
+// let newScore = Number(score)
+// console.table([newScore,typeof newScore])
 console.log("----------------------------------------------------------------------------");
-let isLoggedIn = 1 // Returns true after boolean conversion
+// let isLoggedIn = 1 // Returns true after boolean conversion
 // let isLoggedIn = "  " // Returns true after boolean conversion
 // let isLoggedIn = "sccs" // Returns true after boolean conversion
 // let isLoggedIn = "" // Returns false after boolean conversion
 // let isLoggedIn = 0  // Returns false after boolean conversion
-let boolisLoggedIn = Boolean(isLoggedIn)
-console.table([isLoggedIn,typeof isLoggedIn])
-console.table([boolisLoggedIn,typeof boolisLoggedIn])
+
+// let boolisLoggedIn = Boolean(isLoggedIn)
+// console.table([isLoggedIn,typeof isLoggedIn])
+// console.table([boolisLoggedIn,typeof boolisLoggedIn])
+console.log("----------------------------------------------------------------------------");
+let val = 3
+let newVal = -val
+console.log(newVal);
+
+console.log('2*2 = ',2*2);
+console.log('2/21 = ',2/21);
+console.log('2+2 = ',2+2);
+console.log('2 % 3 = ',2 % 3);
+console.log('2**3 = ',2**3);
+console.log('Math.floor(2/1.1) = ',Math.floor(2/1.1));
+console.log('---------------------------------')
+console.log("" + 1);
+console.log(1 + '');
+console.log(0 + '');
+console.log('' + '0');
+console.log('1' + 1);
+console.log(1 + '1');
+console.log('2' + 1);
+console.log('1' + 2);
+console.log(3 + '2' + 1);
+console.log('3' + 2 + 1);
+console.log('3' + '2' + 1);
+console.log(3 + 2 + '1');
+console.log(3 + '2aaa' + '1');
+console.log(3 + '2aa' );
+
+
+
+
+
+
+
