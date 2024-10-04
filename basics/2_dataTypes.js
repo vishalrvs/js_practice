3b3bcd16d7ce8682223ada66d42b56949b83727a
vishalrvs <32324136+vishalrvs@users.noreply.github.com>
2024-10-04 07:32:50 +0000

    Data Types

---
+++ basics/2_dataTypes.js
@@ -0,0 +1,21 @@
+/* DataTypes and ecma standards */
+"use strict" // Treat all js code as newer version
+
+// alert('Hello') // We are using node js not browser 'ReferenceError: alert is not defined'
+
+console.log(3
+    +3); // Code readability should be high
+
+let age = 31
+let myName = "Vishal"
+let adult = true
+let hieght = 5.3
+let job = null
+// undefined
+// Symbol => unique
+console.log(typeof age)
+console.log(typeof myName)
+console.log(typeof adult)
+console.log(typeof hieght)
+console.log(typeof job)
+console.log(typeof undefined)
