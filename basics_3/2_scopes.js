90fee9853856e9a9d48bde8c9ae509257a64d294
vishalrvs <32324136+vishalrvs@users.noreply.github.com>
2024-10-11 06:15:27 +0000

    2_scopes.js (Demonstrated assign function to variable)

--- basics_3/2_scopes.js
+++ basics_3/2_scopes.js
@@ -17,3 +17,45 @@ if (true) {
     console.log("Displayed Inner d: ",d);
 }
 console.log("Displayed Outer d: ",d);
+
+console.log('**********************Nested Function Scope*************************************');
+
+function one() {
+    let username = "Vishal"
+
+    function two() {
+        const website = 'www.stockinfo.com'
+        console.log(username);
+        
+    }
+    // console.log(website); // Reference error: website is not defined
+    two()
+}
+
+one()
+
+console.log('**********************Nested If Scope*************************************');
+
+
+if (true) {
+    let user = 'User_Vishal'
+    if(user === 'User_Vishal'){
+        const web = "v.com"
+        console.log(user + web);        
+    }
+    // console.log(web);    // ReferenceError: web is not defined
+}
+// console.log(user); // ReferenceError: user is not defined
+
+console.log('**********************Hoisting*************************************');
+
+console.log(addOne(5));
+function addOne(value) {
+    return ++value
+}
+
+// console.log(addTwo(5)); // ReferenceError: Cannot access 'addTwo' before initialization
+let addTwo = function(value) {
+    return value+=2
+}
+console.log(addTwo(5));
