65ee8db82523a532502d79e0ace54430eff30b70
vishalrvs <32324136+vishalrvs@users.noreply.github.com>
2024-10-17 08:08:40 +0000

    1_for.js (Demonstrated simple for loop)

---
+++ loops/1_for.js
@@ -0,0 +1,19 @@
+for (let index = 1; index <= 10; index++) {
+    // console.log(index);
+}
+
+let arr = [112,33,5,11,'sxsx','44']
+let arr1 = new Array(1,2,3,4,5);
+// console.log(arr);
+// console.log(arr1);
+
+for (let index = 0; index < arr.length; index++) {
+    // console.log(arr[index]);        
+}
+
+// console.log(typeof arr1); // Object type
+
+let str = "Vishal"
+for (let index = 0; index < str.length; index++) {
+    // console.log(str[index]);
+}
