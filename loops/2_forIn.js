1896eefd2aa6bd1e1a061d07bee6721fe3ec5b82
vishalrvs <32324136+vishalrvs@users.noreply.github.com>
2024-10-17 08:32:09 +0000

    2_forIn.js (Demonstrated forIn, Not Iterable on Map type, returns key of obj, returns key of array )

---
+++ loops/2_forIn.js
@@ -0,0 +1,24 @@
+let arr = [112,33,5,11,'sxsx','44']
+for (const element in arr) {
+    // console.log(element); // Returns key of array
+}
+
+// let obj = {name: "Vishal", age:31}
+let obj = {name: "Vishal", age:31 , color: {hair : 'black', skin : "fair"}}
+for (const element in obj) {  
+    // console.log(element); // Returns key of object
+}
+
+map1 = new Map()
+// map1.set(['cpp','js','py','vb'],['C plus plus','Java script','Python','Visual Basic'])
+map1.set('cpp','C plus plus')
+map1.set('js','Java Script')
+// console.log(map1); // Map(2) { 'cpp' => 'C plus plus', 'js' => 'Java Script' }
+
+// Not iterable
+for (const [key, ele] in map1) {
+    console.log(key);    
+    console.log(ele);
+}
+
+
